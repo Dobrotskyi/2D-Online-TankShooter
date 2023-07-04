2df1e2ef80379c3dcb72cd1c719755422f25da05
Valeriy <dobrotskiyvd@gmail.com>
2023-07-04 22:27:51 +0300

    Додав кольори статусам.

--- Assets/Scripts/Lobby/LobbyListItem.cs
+++ Assets/Scripts/Lobby/LobbyListItem.cs
@@ -6,6 +6,8 @@ public class LobbyListItem : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _nameField;
     [SerializeField] private TextMeshProUGUI _statusField;
     private PlayerReadyStatus _status = PlayerReadyStatus.Not_Ready;
+    private Color _readyColor = Color.green;
+    private Color _notReadyColor = Color.red;
 
     public PlayerReadyStatus Status => _status;
 
@@ -14,14 +16,21 @@ public class LobbyListItem : MonoBehaviour
         _nameField.text = player.NickName;
         _status = PlayerReadyStatus.Not_Ready;
         _statusField.text = _status.ToString().Replace("_", " ");
+        _statusField.color = _notReadyColor;
     }
 
     public void ChangeStatus()
     {
         if (_status == PlayerReadyStatus.Ready)
+        {
             _status = PlayerReadyStatus.Not_Ready;
+            _statusField.color = _notReadyColor;
+        }
         else if (_status == PlayerReadyStatus.Not_Ready)
+        {
             _status = PlayerReadyStatus.Ready;
+            _statusField.color = _readyColor;
+        }
 
         _statusField.text = _status.ToString().Replace("_", " ");
     }
